1c8ed730559c7c4b4fb915d45aa7ab5e9cf0d0cd
Zachary Brachmanis <zac@kodigosystems.com>
2015-04-14 01:07:03 -0700

    initial commit

--- matrices/main.c
+++ matrices/main.c
@@ -9,7 +9,47 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int array[5][3] = {{1,2,3},{3,4,4},{0,1,1},{5,6,2},{10,1,5}};
+    int captured_points[5][3];
+    int x_array_length = 5;
+    int y_array_length = 3;
+    size_t size = 0;
+    
+    for (int i = 0; i < x_array_length; i++) {
+        for (int j = 0; j < y_array_length; j++) {
+            if (j == 2) {
+                printf("%d",array[i][j]);
+            } else {
+                printf("%d,",array[i][j]);
+            }
+            if (array[i][j] == 0) {
+                printf("points = %d,%d\n",i, j);
+                captured_points[size][0] = i;
+                captured_points[size++][1] = j;
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < x_array_length; j++) {
+            array[captured_points[i][0]][j] = 0;
+        }
+        for (int k = 0; k < y_array_length; k++) {
+            array[k][captured_points[i][1]] = 0;
+        }
+    }
+    
+    for (int i = 0; i < x_array_length; i++) {
+        for (int j = 0; j < y_array_length; j++) {
+            if (j == 2) {
+                printf("%d",array[i][j]);
+            } else {
+                printf("%d,",array[i][j]);
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
